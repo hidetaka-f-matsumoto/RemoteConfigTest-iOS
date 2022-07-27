fe2987d1c8c5a9d97d326829b40c2f1e102eb4df
Hidetaka Matsumoto <tanemomi06@gmail.com>
2022-07-27 23:12:31 +0900

    Create project.

---
+++ RemoteConfigTest/RemoteConfigTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemoteConfigTestApp.swift
+//  RemoteConfigTest
+//
+//  Created by Hidetaka Matsumoto on 2022/07/27.
+//
+
+import SwiftUI
+
+@main
+struct RemoteConfigTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
